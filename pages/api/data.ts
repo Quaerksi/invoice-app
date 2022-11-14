91e590fe1f0055cd67f3bc99c3d774c9ad23f13a
Juliette <juliettesalevsky@gmail.com>
2022-11-14 12:56:26 +0100

    init next.js and create route to json data

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import datas from "../../data.json"
+
+// developement URL http://localhost:3000/api/data
 
 type Data = {
-  name: string
+  data: typeof datas[0][]
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({data: datas})
 }
